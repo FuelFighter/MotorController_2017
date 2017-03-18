070bb51ad756b492339bff6912184633ac20240f
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 14:44:15 +0100

    Added Atmel Studio project and UM driver submodule

---
+++ MotorController_2017/main.c
@@ -0,0 +1,18 @@
+/*
+ * MotorController_2017.c
+ *
+ * Created: 18/03/17 14:39:55
+ * Author : Sondre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
